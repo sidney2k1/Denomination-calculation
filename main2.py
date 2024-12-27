f7b8c41ee3b16a246fdb570e19511cd393470d1b
sidney2k1 <sidsingla02@gmail.com>
2024-12-27 22:21:05 +0530

    main2

--- main2.py
+++ main2.py
@@ -50,4 +50,16 @@ def topwin():
             t3.insert(END,str(note100))
         except ValueError:
             messagebox.showerror("Error","Please enter a valid number")
-            
+    btn=Button(top,text="Calculate",command=calculator,bg="brown",fg="white")
+    label.place(x=230,y=50)
+    entry.place(x=200,y=80)
+    btn.place(x=240,y=120)
+    label.place(x=140,y=170)
+    l1.place(x=180,y=200)
+    l2.place(x=180,y=230)
+    l3.place(x=180,y=260)
+    t1.place(x=270,y=200)
+    t2.place(x=270,y=230)
+    t3.place(x=270,y=260)
+    top.mainloop()
+root.mainloop()
